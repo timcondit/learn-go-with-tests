849d4f71ef0cc2b0086f2e8d0aa6fd5067439afa
Tim Condit <tim.condit@skillfox.com>
2021-05-06 07:58:39 -0700

    Use a const

--- hello/hello.go
+++ hello/hello.go
@@ -2,9 +2,12 @@ package main
 
 import "fmt"
 
+// Tiny performance improvement
+const englishHelloPrefix = "Hello, "
+
 // Separating "domain" code (sending a string) ..
 func Hello(name string) string {
-	return "Hello, " + name
+	return englishHelloPrefix + name
 }
 
 // .. from side effects (printing to stdout)
